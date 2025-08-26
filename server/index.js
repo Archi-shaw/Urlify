fad01a5580290e63bb799e011ffa85659e77b587
Archi Shaw <archishaw2004@gmail.com>
2025-08-26 19:21:44 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const allowedOrigins = [
   'http://localhost:5173', 
+  "https://urlify-backened.onrender.com",
 ];
 
 app.use(cors({
