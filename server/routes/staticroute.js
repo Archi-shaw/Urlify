02c0a58bd42acc3159628ae79aa3854a830ee667
Archi Shaw <archishaw2004@gmail.com>
2025-08-26 19:10:10 +0530

    commit

--- server/routes/staticroute.js
+++ server/routes/staticroute.js
@@ -2,27 +2,25 @@ const express = require('express');
 const URL = require('../models/url');
 
 const router = express.Router();
-
-
-
 router.get('/', async (req, res) => {
-   if (!req.user) {
-    return res.redirect('/signup'); 
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized. Please log in." });
   }
+  try {
     const allUrls = await URL.find({ createdBy: req.user.id });
-    return res.render('home', {
-        urls: allUrls,
-    });
+    return res.json({ urls: allUrls });
+  } catch (err) {
+    console.error("Error fetching URLs:", err);
+    return res.status(500).json({ message: "Server error" });
+  }
 });
 
+router.get('/signup', (req, res) => {
+  return res.json({ message: "Use /user/signup (POST) to register" });
+});
 
-router.get('/signup', async(req,res) => {
-  return res.render('signup');
-})
-
-router.get('/login', async(req,res) => {
-  return  res.render('login');
-})
+router.get('/login', (req, res) => {
+  return res.json({ message: "Use /user/login (POST) to authenticate" });
+});
 
 module.exports = router;
-
